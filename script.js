e8c329ddff866d8dd47dbc2a833a51c506e2fce0
DegrangeM <53106394+DegrangeM@users.noreply.github.com>
2022-05-19 13:12:54 +0200

    Ajout de mini vibration à chaque étape

--- script.js
+++ script.js
@@ -4,18 +4,22 @@ let last = false;
 let timeout = null;
 let speed = location.hash === '#demo' ? 100 : 1;
 let noSleep = new NoSleep(); // empêche l'écran du téléphone de s'éteindre automatiquement
+let etape = 1;
 
 function afficher() {
     if (timer < 20 * 60 * 1000) {
         document.querySelector('.overlay').style.height = (timer / 1000) / (20 * 60) * 100 + '%';
     } else {
         document.querySelector('.overlay').style.height = '100%';
+        etape === 3 && (vibrer([350, 350, 350, 350, 350]), etape = 4);
     }
     if (timer <= 5 * 60 * 1000) {
         document.querySelector('.etape1').textContent = temps(5 * 60 - parseInt(timer / 1000));
     } else if (timer <= 15 * 60 * 1000) {
+        etape === 1 && (vibrer(500), etape = 2);
         document.querySelector('.etape2').textContent = temps(15 * 60 - parseInt(timer / 1000));
     } else {
+        etape === 2 && (vibrer(500), etape = 3);
         document.querySelector('.etape3').textContent = temps(20 * 60 - parseInt(timer / 1000));
     }
 }
